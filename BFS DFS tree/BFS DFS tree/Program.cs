1987305c2db60585e0325289bd586ae0c4e0cea3
Eugene <gitdelatrue@gmail.com>
2021-11-18 18:42:39 +0500

    BFS DFS search on tree

---
+++ BFS DFS tree/BFS DFS tree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BFS_DFS_tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Реализуйте DFS и BFS для дерева с выводом каждого шага в консоль. 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
