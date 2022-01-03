32aab5c4eb3a58a71e0ad5dfd28e43f3f4981ded
Eugene <gitdelatrue@gmail.com>
2022-01-03 21:33:14 +0500

    ДЗ к ур.4

---
+++ OOP4/OOP4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //в процессе решения, будет готово 4-5 января
+        }
+    }
+}
