abce739d889ac9013b72c7824e19f9da3bfaf184
Eugene <gitdelatrue@gmail.com>
2022-01-18 03:56:08 +0500

    ООП.Ур.3

---
+++ OOP2/OOP2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount();
+            account1.IdAccount();
+            account1.Balance = 1000;
+            account1.TypeAccount = TypeAccount.Current;
+            account1.Print();
+
+            BankAccount account2 = new BankAccount();
+            account2.IdAccount();
+            account2.Balance = 2000;
+            account2.TypeAccount = TypeAccount.Current;
+            account2.Print();
+
+            account1.GetMoneyFromAccount(500);
+            account1.PutMoneyToAccount(600);
+
+            account1.TransferTransaction(account2, 500);
+            account1.Print();
+            account2.Print();
+
+            account1.Reverse("swordDROWS");
+        }
+    }
+}
