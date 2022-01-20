cd8d821afa49361deb195fb548e6e7a1bffc28e9
Eugene <gitdelatrue@gmail.com>
2022-01-21 03:52:32 +0500

    update

---
+++ Bucket sort/BucketSort v1.1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BucketSort_v1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BucketSorting bucketSorting = new BucketSorting();
+
+            int[] array = {-3, 12, 56, 23, -8, 120, 8 };
+
+            bucketSorting.BucketSort(ref array);
+
+            foreach(int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }        
+    }
+    public class BucketSorting
+    {
+        public void BucketSort(ref int[] array)
+        {
+            if (array.Length <= 1)
+            {
+                return;
+            }
+
+            int maxValue = array[0];
+            int minValue = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                }
+
+                if (array[i] < minValue)
+                {
+                    minValue = array[i];
+                }                    
+            }
+
+            List<int>[] bucket = new List<int>[maxValue - minValue + 1];
+
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                bucket[array[i] - minValue].Add(array[i]);
+            }
+
+            int index = 0;
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                if (bucket[i].Count > 0)
+                {
+                    for (int j = 0; j < bucket[i].Count; j++)
+                    {
+                        array[index] = bucket[i][j];
+                        index++;
+                    }
+                }
+            }
+        }
+    }
+
+}
