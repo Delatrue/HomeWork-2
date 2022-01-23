44dfe1b4b269967d6559ebf509e33ffc76e43e03
Eugene <gitdelatrue@gmail.com>
2022-01-24 03:47:04 +0500

    OOP.Ур.5

--- OOP5/OOP5/Program.cs
+++ OOP5/OOP5/Program.cs
@@ -6,7 +6,31 @@ namespace OOP5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RationalNum number = new RationalNum(10, 2);
+            RationalNum number2 = new RationalNum(2, 10);
+
+            RationalNum number3 = number / number2;
+            Console.WriteLine(number3);
+
+            number3 = number * number2;
+            Console.WriteLine(number3);
+
+            number3 = number + number2;
+            Console.WriteLine(number3);
+
+            number3 = number - number2;
+            Console.WriteLine(number3);
+
+            number3 = number--;
+            Console.WriteLine(number3);
+
+            number3 = number++;
+            Console.WriteLine(number3);
+
+            Console.WriteLine(number == number2);
+            Console.WriteLine(number < number2);
+            Console.WriteLine(number > number2);
+            Console.WriteLine(number.Equals(number2));
         }
     }
 }
--- OOP5/OOP5/RationalNum.cs
+++ OOP5/OOP5/RationalNum.cs
@@ -8,11 +8,152 @@ namespace OOP5
 {
     internal class RationalNum
     {
-        private int numerator;
-        private int denominator;
-        
-        
+        private double _numerator;
+        private double _denominator;
 
+        public RationalNum(double x, double y)
+        {
+            _numerator = x;
+            _denominator = y;
+        }
+
+        public double X
+        {
+            get { return _numerator; }
+            set { _numerator = value; }
+        }
+        public double Y
+        {
+            get { return _denominator; }
+            set { _denominator = value; }
+        }
+        // ToString()
+        public override string ToString()
+        {
+            return "Дробь: " + _numerator + "/" + _denominator;
+        }
+        // Общий знаменатель
+        public static void _CommonDenomenator(RationalNum num1, RationalNum num2)
+        {
+            if (num1.Y != num2.Y)
+            {
+                num1.X = num1.X * num2.Y;
+                num2.X = num2.X * num1.Y;
+                num1.Y = num1.Y * num2.Y;
+                num2.Y = num1.Y;
+            }
+        }
+        // Перегрузка  !=  ==  Equals  GetHashCode <> <= >=
+        public static bool operator !=(RationalNum num1, RationalNum num2)
+        {
+            _CommonDenomenator(num1, num2);
+            if (num1.X != num2.X)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator ==(RationalNum num1, RationalNum num2)
+        {
+            _CommonDenomenator(num1, num2);
+            if (num1.X == num2.X)
+            {
+                return true;
+            }
+            return false;
+        }
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            RationalNum other = (RationalNum)obj;
+            return _numerator.Equals(other._numerator) && _denominator.Equals(other._denominator);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator <(RationalNum num1, RationalNum num2)
+        {
+            _CommonDenomenator(num1, num2);
+            if (num1.X < num2.X)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >(RationalNum num1, RationalNum num2)
+        {
+            _CommonDenomenator(num1, num2);
+            if (num1.X > num2.X)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator <=(RationalNum num1, RationalNum num2)
+        {
+            _CommonDenomenator(num1, num2);
+            if (num1.X <= num2.X)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >=(RationalNum num1, RationalNum num2)
+        {
+            _CommonDenomenator(num1, num2);
+            if (num1.X >= num2.X)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static RationalNum operator ++(RationalNum num1)
+        {
+            num1.X++;
+            return num1;
+        }
+        public static RationalNum operator --(RationalNum num1)
+        {
+            num1.X--;
+            return num1;
+        }
+
+        public static RationalNum operator +(RationalNum num1, RationalNum num2)
+        {
+            _CommonDenomenator(num1, num2);
+            return new RationalNum(num1.X + num2.X, num1.Y);
+        }
+        public static RationalNum operator -(RationalNum num1, RationalNum num2)
+        {
+            _CommonDenomenator(num1, num2);
+            return new RationalNum(num1.X - num2.X, num1.Y);
+        }
+        public static RationalNum operator *(RationalNum num1, RationalNum num2)
+        {
+            return new RationalNum(num1.X * num2.X, num1.Y * num2.Y);
+        }
+        public static RationalNum operator /(RationalNum num1, RationalNum num2)
+        {
+            return new RationalNum(num1.X * num2.Y, num1.Y * num2.X);
+        }
+        public static RationalNum operator %(RationalNum num1, RationalNum num2)
+        {
+            return new RationalNum(num1.X * num2.Y, num1.Y * num2.X);
+        }
+        public static explicit operator float(RationalNum num1)
+        {
+            return (float)num1;
+        }
+        public static explicit operator int(RationalNum num1)
+        {
+            return (int)num1;
+        }
 
     }
 }
