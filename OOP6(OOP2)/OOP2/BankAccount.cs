073325bd4524503d8e409e24812320bc458a9fab
Eugene <gitdelatrue@gmail.com>
2022-02-19 01:25:28 +0500

    ООП.Ур.6

---
+++ OOP6(OOP2)/OOP2/BankAccount.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    enum TypeAccount
+    {
+        Current,
+        Deposit,
+        Foreign
+    }
+    internal class BankAccount
+    {
+        private int _idAccount;
+        private decimal _balance;
+        private TypeAccount _typeAccount;
+        private static int _idAccountGenerate = 0;
+
+        public int IdAccount()
+        {
+            this._idAccount = ++_idAccountGenerate;
+            return _idAccountGenerate;
+        }
+        public decimal Balance
+        {
+            get
+            {
+                return this._balance;
+            }
+            set
+            {
+                this._balance = value;
+            }
+        }
+        public TypeAccount TypeAccount
+        {
+            get
+            {
+                return this._typeAccount;
+            }
+            set
+            {
+                this._typeAccount = value;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{_idAccount} {_balance} {_typeAccount}");
+        }
+
+        public decimal GetMoneyFromAccount(int value)
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Operation error");
+                return -1;
+            }
+            else if (_balance - value > 0)
+            {
+                _balance = _balance - value;
+                Console.WriteLine($"Остаток на счете {_balance}");
+                return _balance;
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств");
+                return -1;
+            }
+        }
+
+        public decimal PutMoneyToAccount(int value)
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Operation error");
+                return -1;
+            }
+            else
+            {
+                _balance = _balance + value;
+                Console.WriteLine($"Остаток на счете {_balance}");
+                return _balance;
+            }
+        }
+
+        public void TransferTransaction(BankAccount fromAccount, int sum)
+        {
+            if (fromAccount is null || fromAccount.Balance < sum)
+            {
+                return;
+            }
+            fromAccount.Balance -= sum;
+            Balance += sum;
+        }
+        public void Reverse(string someString)
+        {
+            char[] newChar = someString.ToCharArray();
+            Array.Reverse(newChar);
+            Console.WriteLine($"{new string(newChar)}");
+        }
+        public static bool operator ==(BankAccount account1, BankAccount account2)
+        {
+            if (account1.Balance == account2.Balance)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(BankAccount account1, BankAccount account2)
+        {
+            if (account1.Balance != account2.Balance)
+            {
+                return true;
+            }
+            return false;
+        }
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            BankAccount other = (BankAccount)obj;
+            return _balance.Equals(other._balance) && _balance.Equals(other._balance);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return "Счет: №" + _idAccount + " (" + _typeAccount + ") Баланс: " + _balance;
+        }
+    }
+}
---
+++ OOP6(OOP2)/OOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount();
+            account1.IdAccount();
+            account1.Balance = 1000;
+            account1.TypeAccount = TypeAccount.Current;
+            //account1.Print();
+
+            BankAccount account2 = new BankAccount();
+            account2.IdAccount();
+            account2.Balance = 2000;
+            account2.TypeAccount = TypeAccount.Current;
+            //account2.Print();
+
+            //account1.GetMoneyFromAccount(500);
+            //account1.PutMoneyToAccount(600);
+
+            //account1.TransferTransaction(account2, 500);
+            //account1.Print();
+            //account2.Print();
+
+            //account1.Reverse("swordDROWS");
+
+            Console.WriteLine(account1);
+            Console.WriteLine(account2);
+            Console.WriteLine(account1 == account2);
+            Console.WriteLine(account2 != account1);
+            Console.WriteLine(account1.Equals(account2));
+            Console.WriteLine(account1.GetHashCode());
+        }
+    }
+}
